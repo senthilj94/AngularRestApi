c67d9c64e9dc3e424e8cb75e45eec2ca7c7d3530
senthilj94 <senthilj94@users.noreply.github.com>
2020-04-01 21:30:32 +0530

    pipe changes

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,16 @@ export class EventsService {
   }
 
   getSpecialEvents(): Observable<any> {
-    return this.http.get(`${this.eventsUrl}${'getspecialevents'}`);
+    return this.http.get(`${this.eventsUrl}${'getspecialevents'}`)
+    .pipe(catchError(this.handleError));
   }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    if (errorResponse.error instanceof ErrorEvent) {
+        console.error('Client Side Error :', errorResponse.error.message);
+    } else {
+        console.error('Server Side Error :', errorResponse);
+    }
+    return throwError('There is a problem with the service. We are notified & working on it. Please try again later.');
+}
 }
